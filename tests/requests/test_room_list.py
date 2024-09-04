5c6e3098b6e5e2bd7e9f696b00b85a8735eae451
aohus <dbtnghk528@gmail.com>
2024-09-04 18:31:07 +0900

    feature: error handling for req/res

--- tests/requests/test_room_list.py
+++ tests/requests/test_room_list.py
@@ -1,13 +1,58 @@
-from rentomatic.requests.room_list import RoomListRequest
+import pytest
+
+from rentomatic.requests.room_list import build_room_list_request
 
 
 def test_build_room_list_request_without_parameters():
-    request = RoomListRequest()
+    request = build_room_list_request()
 
+    assert request.filters is None
     assert bool(request) is True
 
 
-def test_build_room_list_request_from_empty_dict():
-    request = RoomListRequest.from_dict({})
+def test_build_room_list_request_with_empty_filters():
+    request = build_room_list_request({})
 
+    assert request.filters == {}
     assert bool(request) is True
+
+
+# 잘못된 타입을 가진 객체를 필터로 전달하는 경우와 잘못된 키를 사용하는 요청에 대한 테스트
+def test_build_room_list_request_with_invalid_filters_parameter():
+    request = build_room_list_request(filters=5)
+
+    assert request.has_errors()
+    assert request.errors[0]["parameter"] == "filters"
+    assert bool(request) is False
+
+
+def test_build_room_list_request_with_incorrect_filter_keys():
+    request = build_room_list_request(filters={"a": 1})
+
+    assert request.has_errors()
+    assert request.errors[0]["parameter"] == "filters"
+    assert bool(request) is False
+
+
+# 지원하는 키와 지원하지 않는 키를 사용하는 테스트
+@pytest.mark.parametrize(
+    "key", ["code__eq", "price__eq", "price__lt", "price__gt"]
+)
+def test_build_room_list_request_accepted_filters(key):
+    filters = {key: 1}
+
+    request = build_room_list_request(filters=filters)
+
+    assert request.filters == filters
+    assert bool(request) is True
+
+
+@pytest.mark.parametrize("key", ["code__lt", "code__gt"])
+def test_build_room_list_request_rejected_filters(key):
+    filters = {key: 1}
+
+    request = build_room_list_request(filters=filters)
+
+    assert request.has_errors()
+    assert request.errors[0]["parameter"] == "filters"
+    assert bool(request) is False
