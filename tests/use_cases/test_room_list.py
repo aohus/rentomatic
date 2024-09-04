dad42133f810b24d416f36593f3d8eb1ec0d0927
aohus <dbtnghk528@gmail.com>
2024-09-04 18:50:53 +0900

    feature: update use_cases for check response validation

--- tests/use_cases/test_room_list.py
+++ tests/use_cases/test_room_list.py
@@ -5,6 +5,7 @@ import pytest
 
 from rentomatic.domain.room import Room
 from rentomatic.requests.room_list import build_room_list_request
+from rentomatic.responses import ResponseTypes
 from rentomatic.use_cases.room_list import room_list_use_case
 
 
@@ -54,5 +55,34 @@ def test_room_list_without_parameters(domain_rooms):
     response = room_list_use_case(repo, request)
 
     assert bool(response) is True
-    repo.list.assert_called_with()
+    repo.list.assert_called_with(filters=None)
     assert response.value == domain_rooms
+
+
+def test_room_list_with_filters(domain_rooms):
+    repo = mock.Mock()
+    repo.list.return_value = domain_rooms
+
+    qry_filters = {"code__eq": 5}
+    request = build_room_list_request(filters=qry_filters)
+
+    response = room_list_use_case(repo, request)
+
+    assert bool(response) is True
+    repo.list.assert_called_with(filters=qry_filters)
+    assert response.value == domain_rooms
+
+
+def test_room_list_handles_generic_error():
+    repo = mock.Mock()
+    repo.list.side_effect = Exception("Just an error message")
+
+    request = build_room_list_request(filters={})
+
+    response = room_list_use_case(repo, request)
+
+    assert bool(response) is False
+    assert response.value == {
+        "type": ResponseTypes.SYSTEM_ERROR,
+        "message": "Exception: Just an error message",
+    }
